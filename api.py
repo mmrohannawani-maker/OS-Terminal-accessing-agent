4e03eb5c80daddc66233e037bf53b30bc0b5b42c
mmrohannawani-maker <mm.rohannawani@gmail.com>
2026-02-25 12:26:59 +0530

    added links

--- api.py
+++ api.py
@@ -34,9 +34,10 @@ app = FastAPI()
 # =====================================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vibrant-patience-production-68b7.up.railway.app",
-        "http://localhost:5173",
-        "http://localhost:8000"],
+    allow_origins=["https://vibrant-patience-production-68b7.up.railway.app/",
+                   "https://os-terminal-accessing-agent-production.up.railway.app/",
+                   "http://localhost:5173",
+                   "http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
